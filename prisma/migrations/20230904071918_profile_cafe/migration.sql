671cf5a08780e8263e1c5f876bfaddec1ca7939c
Haziq Musa <last.game01987@gmail.com>
2023-09-04 15:26:03 +0800

    change relation

---
+++ prisma/migrations/20230904071918_profile_cafe/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[userId]` on the table `Cafe` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[userId]` on the table `Profile` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "Cafe_userId_key" ON "Cafe"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
