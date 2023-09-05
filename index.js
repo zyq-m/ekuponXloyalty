f2f166ad03952a52c229d626f2caeaf029c35917
Haziq Musa <last.game01987@gmail.com>
2023-09-05 15:56:23 +0800

    admin route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const student = require("./src/routes/studentRoute");
 const cafe = require("./src/routes/cafeRoute");
+const admin = require("./src/routes/adminRoute");
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/student", student);
 app.use("/api/cafe", cafe);
+app.use("/api/admin", admin);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
