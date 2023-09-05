105ed0ba53c4ba936e682cdc1d24774999341c1b
Haziq Musa <last.game01987@gmail.com>
2023-09-05 12:06:55 +0800

    change to .ts

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,13 +1,14 @@
-const bcrypt = require("bcryptjs");
+import bcrypt from "bcryptjs";
+
 const salt = bcrypt.genSaltSync(10);
 
 // Hash password
-exports.hash = function (password) {
+export const hash = function (password: string) {
   return bcrypt.hashSync(password, salt);
 };
 
 // Check password
-exports.check = function (password, hashed) {
+export const check = function (password: string, hashed: string) {
   // compare plain password with hashed
   return bcrypt.compareSync(password, hashed); // true
 };
