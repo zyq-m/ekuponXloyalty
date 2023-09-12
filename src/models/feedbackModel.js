cbacb7ae3567fda658c0448fe5eb66957600a072
Haziq Musa <last.game01987@gmail.com>
2023-09-13 00:22:05 +0800

    refactor feedback

--- src/models/feedbackModel.js
+++ src/models/feedbackModel.js
@@ -1,6 +1,4 @@
 const { PrismaClient } = require("@prisma/client");
-const studentModel = require("./studentModel");
-const cafeModel = require("./cafeModel");
 
 const prisma = new PrismaClient();
 
@@ -13,25 +11,4 @@ exports.save = async (id, description) => {
       description: description,
     },
   });
-};
-
-exports.getAll = async () => {
-  return await prisma.feedback.findMany();
-};
-
-// HELPER
-const findId = async id => {
-  const student = studentModel.getUserId(id);
-  const cafe = cafeModel.getUserId(id);
-
-  const userId = await Promise.allSettled([student, cafe]);
-
-  if (!userId[0] && !userId[1]) {
-    return undefined;
-  }
-
-  if (userId[0]) {
-    return userId[0];
-  }
-  return userId[1];
 };
