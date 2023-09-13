7003ed3c9cfdd7ccd0470aaa93c7bca51192fc61
Haziq Musa <last.game01987@gmail.com>
2023-09-13 22:49:00 +0800

    create logout & updateAccessToken func

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
-const { getUser, storeRefreshToken } = require("../models/useModel");
-const { generateToken } = require("../services/jwt");
+const { getUser } = require("../models/useModel");
+const userTokenModel = require("../models/userTokenModel");
+const jwt = require("../services/jwt");
 const { check } = require("../utils/bcrypt");
 
 exports.login = async (req, res) => {
@@ -20,11 +21,49 @@ exports.login = async (req, res) => {
     }
 
     // Generate token
-    const { accessToken, refreshToken } = generateToken(user);
-    await storeRefreshToken(refreshToken, user.id);
+    const accessToken = jwt.generateAccessToken(user);
+    const refreshToken = jwt.generateRefreshToken(user);
+    // Store refresh token in db
+    await userTokenModel.storeRefreshToken(refreshToken, user.id);
 
     return res.status(200).send({ accessToken, refreshToken });
   } catch (error) {
     return res.status(500).send({ error: error });
+  }
+};
+
+exports.logout = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  try {
+    // remove refresh token
+    const isRemove = await userTokenModel.removeRefreshToken(refreshToken);
+
+    if (!isRemove) {
+      return res.status(404).send({ message: "Token not found" });
+    }
+
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+};
+
+exports.updateAccessToken = async (req, res) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.sendStatus(403);
+  }
+
+  // Verify refresh token
+  try {
+    // decode contain information like payload, in token
+    const decoded = jwt.verifyRefreshToken(refreshToken);
+    const newAccessToken = jwt.generateAccessToken(decoded);
+
+    return res.status(201).send({ accessToken: newAccessToken });
+  } catch (error) {
+    return res.sendStatus(403);
   }
 };
