5f95ca23a6591b0f9fc7c5adf29259bf01bcfccb
Haziq Musa <last.game01987@gmail.com>
2023-09-26 14:45:01 +0800

    refactor middleware

--- src/middlewares/collectPoint.js
+++ src/middlewares/collectPoint.js
@@ -1,46 +1,44 @@
 const { PrismaClient } = require("@prisma/client");
-const { verifyPoint } = require("../services/jwt");
 const { verify } = require("../utils/otp");
 
 // Student can scan one-time QR (token)
 // Student (ios) can input OTP
 exports.verifyPoint = async (req, res, next) => {
-  const { cafeId, token, otp } = req.body;
+  const { cafeId, otp } = req.body;
 
   // Token or OTP must be provided
-  if ((!token || otp) && (token || !otp)) {
+  if (!otp) {
     return res.status(400).send({ message: "Please provide credential" });
   }
 
-  // Verify token (one-time-URL) if exists
-  if (token) {
-    try {
-      verifyPoint(token);
-      next();
-    } catch (error) {
-      console.log(error);
-      return res.status(400).json({ message: "URL expired", error: error });
-    }
-  }
-
   // Verify OTP if exists
-  if (otp) {
-    const prisma = new PrismaClient();
-    const secret = await prisma.sale.findUnique({
-      select: {
-        otp: true,
-      },
-      where: {
-        cafeId: cafeId,
-      },
-    });
+  const prisma = new PrismaClient();
+  const secret = await prisma.sale.findUnique({
+    select: {
+      otp: true,
+    },
+    where: {
+      cafeId: cafeId,
+    },
+  });
 
-    const isVerified = verify(otp, secret.otp);
+  const isVerified = verify(otp, secret.otp);
 
-    if (!isVerified) {
-      return res.status(400).json({ message: "OTP expired" });
-    }
-
-    next();
+  if (!isVerified) {
+    return res.status(400).json({ message: "Credential expired" });
   }
+
+  // Check if otp has been used
+  const prevOtp = await prisma.userToken.findUnique({
+    where: {
+      token: otp,
+      mark: "otp-token",
+    },
+  });
+
+  if (prevOtp) {
+    return res.status(400).send({ message: "Credential has been used" });
+  }
+
+  next();
 };
