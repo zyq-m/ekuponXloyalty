2d33b9b0a0aed421cf309513a9f66c7e1dbca890
zyq-m <last.game01987@gmail.com>
2023-11-02 11:38:52 +0800

    update admin event

--- src/services/socket.io/adminEvent.js
+++ src/services/socket.io/adminEvent.js
@@ -2,27 +2,25 @@ const studentModel = require("../../models/studentModel");
 const cafeModel = require("../../models/cafeModel");
 const transactionModel = require("../../models/transactionModel");
 
-module.exports = (io, socket) => {
+module.exports = async (io, socket) => {
   // Get overall student, cafe & transaction
-  io.emit("admin:get-overall", async () => {
-    const student = studentModel.total();
-    const cafe = cafeModel.total();
-    const coupon = transactionModel.totalCoupon();
-    const point = transactionModel.totalPoint();
-    const arrReq = [student, cafe, coupon, point];
+  const student = studentModel.total();
+  const cafe = cafeModel.total();
+  const coupon = transactionModel.totalCoupon();
+  const point = transactionModel.totalPoint();
+  const arrReq = [student, cafe, coupon, point];
 
-    try {
-      const data = await Promise.all(arrReq);
-      const overall = {
-        student: data[0],
-        cafe: data[1],
-        coupon: data[2],
-        point: data[3],
-      };
+  try {
+    const data = await Promise.all(arrReq);
+    const overall = {
+      student: data[0],
+      cafe: data[1],
+      coupon: data[2],
+      point: data[3],
+    };
 
-      return overall;
-    } catch (error) {
-      return { error: error };
-    }
-  });
+    io.emit("admin:get-overall", overall);
+  } catch (error) {
+    io.emit("admin:get-overall", { error: error });
+  }
 };
